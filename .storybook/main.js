b10997e9e93959511ea75e2185bb4fcf13ee3520
Jun <jun.juno@kakaostyle.com>
2022-12-16 21:10:56 +0900

    feat: webpack import path alias 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  // NOTE: Storybook이 story 파일들을 읽어오기 위해, story가 위치하는 폴더 경로를 glob 패턴으로 명시해주었어요.
+  // components, pages 등의 폴더 하위가 아닌 다른 폴더 아래에도 story 파일을 만들고 싶으면, 여기에 추가해주면 돼요.
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
@@ -11,6 +13,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  // NOTE: next/link, next/router등 nextjs와 호환되는 공식 integration framework를 설치해주었어요.
   framework: {
     name: "@storybook/nextjs",
     options: {},
@@ -20,5 +23,14 @@ module.exports = {
   },
   docs: {
     docsPage: true,
+  },
+  // NOTE: Storybook은 UI를 테스트할 수 있는 독립적인 app이에요. webpackFinal 이라는 속성으로
+  // story들의 webpack 설정을 변경/추가할 수 있어요. 여기선 import 부분을 alias로 처리하기 위한 설정을 해주었어요.
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      "@": path.resolve(__dirname),
+    };
+
+    return config;
   },
 };
--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
